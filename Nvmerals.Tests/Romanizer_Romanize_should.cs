a9d6767b43a291d1957af84c318c0b1aa45c0964
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-07-14 08:47:17 -0500

    C

--- Nvmerals.Tests/Romanizer_Romanize_should.cs
+++ Nvmerals.Tests/Romanizer_Romanize_should.cs
@@ -22,6 +22,7 @@ namespace Nvmerals.Tests
         [InlineData(40,"XL")]
         [InlineData(50,"L")]
         [InlineData(90,"XC")]
+        [InlineData(100,"C")]
         public void convert_integers_into_roman_numeral_equivalent(int givenValue, string expectedResult)
         {
             var actual = Romanizer.Romanize(givenValue);
