fb5259e87d4a73c622ff9e6aba243f467ab8cbf5
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-07-14 07:33:44 -0500

    V!

--- Nvmerals.Tests/Romanizer_Romanize_should.cs
+++ Nvmerals.Tests/Romanizer_Romanize_should.cs
@@ -9,6 +9,7 @@ namespace Nvmerals.Tests
         [InlineData(2,"II")]
         [InlineData(3,"III")]
         [InlineData(4,"IV")]
+        [InlineData(5,"V")]
         public void convert_integers_into_roman_numeral_equivalent(int givenValue, string expectedResult)
         {
             Romanizer.Romanize(givenValue).ShouldBe(expectedResult);
